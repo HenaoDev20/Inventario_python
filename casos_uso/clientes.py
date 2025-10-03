d4c0c2c90a4bb2ce102bbcd41888bb31f2ee1dc9
HenaoDev20 <brianhenao907@gmail.com>
2025-10-02 23:55:39 -0500

    commit

--- casos_uso/clientes.py
+++ casos_uso/clientes.py
@@ -1,16 +1,3 @@
-class Cliente:
-    def __init__(self, id: int, nombre: str, email: str):
-        self.id = id
-        self.nombre = nombre
-        self.email = email
-
-
-def __repr__(self):
-        return f"<Cliente {self.id} - {self.nombre}>"
-
-def validar_email(self):
-        if "@" not in self.email:
-            raise ValueError("El email del cliente no es válido")
 
 
 
